30f89c18144d8ca125142e4f7656e2c2ee14917b
AngyNus <60838153+AngyNus@users.noreply.github.com>
2020-10-24 21:18:51 +0300

    small sentiment analysis course work

---
+++ analysis.py
@@ -0,0 +1,35 @@
+
+"""
+This sample project using nltk.sentiment.vader package
+and googletrans package to translate 30 Facebook statuses from Hebrew to English and define their level of the sentiments this status want to express.
+If there are more negative or positive words, and what
+aproximatly will be the feelling of the avarage human reading this status.
+
+"""
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+from googletrans import Translator
+
+
+nltk.download('vader_lexicon')
+sid = SentimentIntensityAnalyzer()
+
+
+translator = Translator()
+
+# open statuses file
+with open("status.txt", 'r', encoding='utf-8') as status:
+    # string for the complete status
+    sts = " "
+    count = 1
+    for line in status:
+        he_sentence = translator.translate(line)
+        sts += he_sentence.text
+        # if line == '\n':
+        if len(line.strip()) == 0:
+            print(count)
+            print(sts)
+            print(sid.polarity_scores(sts), '\n')
+            count += 1
+            sts = " "
